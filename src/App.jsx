1e0355fa90193cd6babee9ed5c07c5f82e3ab2d1
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2024-07-04 02:11:00 +0500

    Changes to some files add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import React, { lazy, Suspense } from 'react';
 import CurrentStack from './Components/CurrentStack';
 import Footer from './Components/Footer';
 import HeroSection from './Components/HeroSection';
@@ -8,36 +8,41 @@ import Projects from './Components/Projects';
 import SubHero from './Components/SubHero';
 import WorkExp from './Components/WorkExp';
 import NoPage from './Path/NoPage';
-import About from './Path/About';
-import ProjectLink from './Path/ProjectLink';
+import Loading from './Components/Loading';
+
+// Lazy load the About and ProjectLink components
+const About = lazy(() => import('./Path/About'));
+const ProjectLink = lazy(() => import('./Path/ProjectLink'));
+
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route
-            exact
-            path='/'
-            element={
-              <>
-                <HeroSection />
-                <SubHero />
-                <WorkExp />
-                <CurrentStack />
-                <Projects />
-              </>
-            }
-          />
-
-          <Route path='/about' element={<About />} />
-          <Route path='/project' element={<ProjectLink />} />
-          <Route path='*' element={<NoPage />} />
-        </Routes>
-
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route
+              exact
+              path='/'
+              element={
+                <>
+                  <HeroSection />
+                  <SubHero />
+                  <WorkExp />
+                  <CurrentStack />
+                  <Projects />
+                </>
+              }
+            />
+            <Route path='/about' element={<About />} />
+            <Route path='/project' element={<ProjectLink />} />
+            <Route path='*' element={<NoPage />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </BrowserRouter>
     </>
   );
 };
+
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import Loading from './Components/Loading.jsx';
+
+const Main = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return <div>{isLoading ? <Loading /> : <App />}</div>;
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
